a9fbd16ab4fbabe0714c424b102ecef5f0131cf2
RafaelPA13 <84384192+RafaelPA13@users.noreply.github.com>
2022-03-30 12:18:51 -0300

    Add files via upload

---
+++ config/multer.js
@@ -0,0 +1,33 @@
+const multer = require("multer")
+
+const armazenamento = multer.diskStorage(
+    {
+        destination:(req,file,cb)=>{
+            cb(null,'./uploads/')
+        },
+        filename:(req,file,cb)=>{
+            cb(null,Date.now()+file.originalname)
+        }
+    }
+)
+var upload = multer(
+    {
+        storage:armazenamento,
+        limits:{fileSize:100000},
+        fileFilter:(req,file,cb)=>{
+            if(
+                file.mimetype == "images/png" ||
+                file.mimetype == "images/jpg" ||
+                file.mimetype == "images/jpeg"
+             ){
+                cb(null,true)
+             }
+             else{
+                 cb(null,false)
+                 return cb(new Error('Tipo de arquivo inválido'))
+             }
+        }
+    })
+
+module.exports = upload
+
